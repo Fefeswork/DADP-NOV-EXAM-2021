d314ea7b5405ac6d7faf0c7f97d27ff1157d4c03
Fefeswork <2306258@students.wits.ac.za>
2021-12-02 10:53:12 +0200

    iteration10

--- 2306258_DADP NOV EXAM/Assets/Scripts/InkManager.cs
+++ 2306258_DADP NOV EXAM/Assets/Scripts/InkManager.cs
@@ -25,6 +25,17 @@ public class InkManager : MonoBehaviour
     [SerializeField]
     private Color _thoughtTextColor;
 
+    private int _mentalHealth;
+    public int MentalHealth
+    {
+        get => _mentalHealth;
+        private set
+        {
+            Debug.Log($"Updating MentalHealth value. Old value: {_mentalHealth}, new value: {value}");
+            _mentalHealth = value;
+        }
+    }
+
     public Button vfx;
     public Transform BtnRoom;
 
@@ -37,92 +48,109 @@ public class InkManager : MonoBehaviour
     private void StartStory()
     {
         _story = new Story(_inkJsonAsset.text);
+        var mentalHealth = (int)_story.variablesState["mental_health"];
+
+        Debug.Log($"Logging ink variables. mental health: {mentalHealth}");
         DisplayNextLine();
     }
+  
 
-    public void DisplayNextLine()
+    private void InitializeVariables()
     {
-        if (_story.canContinue)
+
+        MentalHealth = (int)_story.variablesState["mental_health"];
+
+        _story.ObserveVariable("mental_health", (arg, value) =>
         {
-            string text = _story.Continue(); // gets next line
-
-            text = text?.Trim(); // removes white space from text
-
-            ApplyStyling();
-
-            _textField.text = text; // displays new text
-        }
-        else if (_story.currentChoices.Count > 0)
-        {
-            DisplayChoices();
-        }
-        else if (_story.canContinue == false)
-        {
-            Debug.Log("Story has ended!");
-            Instantiate(vfx,BtnRoom.transform.position, Quaternion.identity);
-
-
-        }
+            MentalHealth = (int)value;
+            Debug.Log("value changing");
+        });
     }
 
-    private void DisplayChoices()
-    {
-        // checks if choices are already being displaye
-        if (_choiceButtonContainer.GetComponentsInChildren<Button>().Length > 0) return;
-
-        for (int i = 0; i < _story.currentChoices.Count; i++) // iterates through all choices
+        public void DisplayNextLine()
         {
-
-            var choice = _story.currentChoices[i];
-            var button = CreateChoiceButton(choice.text); // creates a choice button
-
-            button.onClick.AddListener(() => OnClickChoiceButton(choice));
-        }
-    }
-
-    Button CreateChoiceButton(string text)
-    {
-        // creates the button from a prefab
-        var choiceButton = Instantiate(_choiceButtonPrefab);
-        choiceButton.transform.SetParent(_choiceButtonContainer.transform, false);
-
-        // sets text on the button
-        var buttonText = choiceButton.GetComponentInChildren<Text>();
-        buttonText.text = text;
-
-        return choiceButton;
-    }
-
-    void OnClickChoiceButton(Choice choice)
-    {
-        _story.ChooseChoiceIndex(choice.index); // tells ink which choice was selected
-        RefreshChoiceView(); // removes choices from the screen
-        DisplayNextLine();
-
-    }
-
-    void RefreshChoiceView()
-    {
-        if (_choiceButtonContainer != null)
-        {
-            foreach (var button in _choiceButtonContainer.GetComponentsInChildren<Button>())
+            if (_story.canContinue)
             {
-                Destroy(button.gameObject);
+                string text = _story.Continue(); // gets next line
+
+                text = text?.Trim(); // removes white space from text
+
+                ApplyStyling();
+
+                _textField.text = text; // displays new text
+            }
+            else if (_story.currentChoices.Count > 0)
+            {
+                DisplayChoices();
+            }
+            else if (_story.canContinue == false)
+            {
+                Debug.Log("Story has ended!");
+                Instantiate(vfx, BtnRoom.transform.position, Quaternion.identity);
+
+
+            }
+        }
+
+        public void DisplayChoices()
+        {
+            // checks if choices are already being displaye
+            if (_choiceButtonContainer.GetComponentsInChildren<Button>().Length > 0) return;
+
+            for (int i = 0; i < _story.currentChoices.Count; i++) // iterates through all choices
+            {
+
+                var choice = _story.currentChoices[i];
+                var button = CreateChoiceButton(choice.text); // creates a choice button
+
+                button.onClick.AddListener(() => OnClickChoiceButton(choice));
+            }
+        }
+
+        Button CreateChoiceButton(string text)
+        {
+            // creates the button from a prefab
+            var choiceButton = Instantiate(_choiceButtonPrefab);
+            choiceButton.transform.SetParent(_choiceButtonContainer.transform, false);
+
+            // sets text on the button
+            var buttonText = choiceButton.GetComponentInChildren<Text>();
+            buttonText.text = text;
+
+            return choiceButton;
+        }
+
+        void OnClickChoiceButton(Choice choice)
+        {
+            _story.ChooseChoiceIndex(choice.index); // tells ink which choice was selected
+            RefreshChoiceView(); // removes choices from the screen
+            DisplayNextLine();
+
+        }
+
+        void RefreshChoiceView()
+        {
+            if (_choiceButtonContainer != null)
+            {
+                foreach (var button in _choiceButtonContainer.GetComponentsInChildren<Button>())
+                {
+                    Destroy(button.gameObject);
+                }
+            }
+        }
+
+        public void ApplyStyling()
+        {
+            if (_story.currentTags.Contains("thought"))
+            {
+                _textField.color = _thoughtTextColor;
+                _textField.fontStyle = FontStyle.Italic;
+            }
+            else
+            {
+                _textField.color = _normalTextColor;
+                _textField.fontStyle = FontStyle.Normal;
             }
         }
     }
 
-    private void ApplyStyling()
-    {
-        if (_story.currentTags.Contains("thought"))
-        {
-            _textField.color = _thoughtTextColor;
-            _textField.fontStyle = FontStyle.Italic;
-        }
-        else
-        {
-            _textField.color = _normalTextColor;
-            _textField.fontStyle = FontStyle.Normal;
-        }
-    }
-}
